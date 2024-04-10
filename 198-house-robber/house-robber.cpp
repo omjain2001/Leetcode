eb621b52219e032c2ec59285eeb92639ac809929
Om Jain <60605251+omjain2001@users.noreply.github.com>
2024-04-10 16:33:04 -0400

    Time: 0 ms (100.00%) | Memory: 9.2 MB (57.83%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -6,7 +6,16 @@ public:
         return arr[i] = max(nums[i] + recur(nums,i-2,arr), recur(nums,i-1,arr));
     }
     int rob(vector<int>& nums) {
-        vector<int> arr(nums.size(),-1);
-        return recur(nums,nums.size()-1, arr);
+        vector<int> arr(nums.size()+1,-1);
+        // return recur(nums,nums.size()-1, arr);
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0], nums[1]);
+        arr[0] = 0;
+        arr[1] = nums[0];
+        for(int i=2; i<=nums.size(); i++){
+            arr[i] = max(nums[i-1] + arr[i-2], arr[i-1]);
+        }
+        return arr[nums.size()];
     }
 };
