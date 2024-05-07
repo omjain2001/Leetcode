dce87761f1c7f9b1c01fb4643b785f19825d3972
Om Jain <60605251+omjain2001@users.noreply.github.com>
2024-05-07 16:17:59 -0400

    Time: 2 ms (59.73%) | Memory: 7.1 MB (82.98%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,17 +1,24 @@
 class Solution {
 public:
+    long long dp[50][50];
     int combinations(int st, int e){
         if(st > e) return 1;
         if(st == e) return 1;
+        if(dp[st][e] != -1) return dp[st][e];
         int sum = 0;
         for(int i=st; i<=e; i++){
-            int leftCom = combinations(st,i-1);
-            int rightCom = combinations(i+1,e);
+            int leftCom = dp[st][i-1] = combinations(st,i-1);
+            int rightCom = dp[i+1][e] = combinations(i+1,e);
             sum += (leftCom * rightCom);
         }
-        return sum;
+        return dp[st][e] = sum;
     }
     int numTrees(int n) {
+        for(int i=0; i<20; i++){
+            for(int j=0; j<20; j++){
+                dp[i][j] = -1;
+            }
+        }
         return combinations(1,n);
     }
 };
