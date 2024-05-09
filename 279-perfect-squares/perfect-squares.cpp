bf373c24962fb65c4313a6e71ed2cd6d402656c1
Om Jain <60605251+omjain2001@users.noreply.github.com>
2024-05-08 21:53:33 -0400

    Time: 33 ms (89.09%) | Memory: 7.3 MB (98.78%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -11,8 +11,23 @@ public:
         return dp[n-1][target-1] = min(ans1, recur(n-1,target,dp));
     }
     int numSquares(int n) {
-        int num = (int)floor(sqrt(n));
-        vector<vector<int>> dp(num,vector<int>(n, -1));
-        return recur(num,n,dp);
+
+        // Approach 1
+        // int num = (int)floor(sqrt(n));
+        // vector<vector<int>> dp(num,vector<int>(n, -1));
+        // return recur(num,n,dp);
+
+        // Approach 2
+        int dp[n + 1];
+        for (int i = 0; i <= n; ++i) {
+            dp[i] = i;
+        }
+        int sq = sqrt(n);
+        for (int i = 2; i <= sq; ++i) {
+            for (int j = i * i; j <= n; ++j) {
+                dp[j] = min(dp[j], dp[j - i * i] + 1);
+            }
+        }
+        return dp[n];
     }
 };
