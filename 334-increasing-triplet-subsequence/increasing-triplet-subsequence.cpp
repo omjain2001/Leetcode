6ab323f48a1a77fb74a7a75abc979d29f16ed88c
Om Jain <60605251+omjain2001@users.noreply.github.com>
2024-05-23 21:49:32 -0400

    Time: 79 ms (80.17%) | Memory: 114.1 MB (65.75%) - LeetSync

---
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int left = INT_MAX;
+        int mid = INT_MAX;
+        if(nums.size() < 3) return false;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i] > mid) return true;
+            else if(nums[i] > left && nums[i] <= mid) mid = nums[i];
+            else left = nums[i];
+        }
+        return false;
+    }
+};
