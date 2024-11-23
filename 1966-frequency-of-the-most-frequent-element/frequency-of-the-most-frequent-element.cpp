0fe71bbdc773934cd615e2d2aea0d0aadfe33815
Om Jain <60605251+omjain2001@users.noreply.github.com>
2024-11-22 19:42:09 -0500

    Time: 36 ms (75.03%) | Memory: 105.9 MB (45.76%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
-    int recur(vector<int> &nums, int i, int k, int target, vector<vector<int>> &dp){
-        if(k == 0 || target-nums[i] > k) return 0;
-        if(i == 0){
-            if(target-nums[i] <= k) return 1;
-            return 0;
-        }
-        if(dp[i][k-1] != -1) return dp[i][k-1];
-        return dp[i][k-1] = 1 + recur(nums,i-1,k-(target-nums[i]),target,dp);
-    }
     int maxFrequency(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        int n = nums.size();
-        long long left = 0, right = 0, res = 0, total = 0;
-        while(right < n){
-            total += nums[right];
-            while(nums[right] * (right-left+1) > total+k){
-                total-=nums[left];
-                left++;
-            }
-            res = max(res, (right-left+1));
-            right++;
+        long long i=0, j=0, sum=0, ans=1;
+        sort(nums.begin(), nums.end());
+        while(j < nums.size()){
+            sum += nums[j];
+            while(i < j && ((j-i+1)*nums[j] - sum) > k) sum -= nums[i++];
+            ans = max(ans, (j-i+1));
+            j++;
         }
-        return res;
+        return ans;
     }
 };
