55d2aac067e51777c48b3bfbdc89758c50c4f782
Om Jain <60605251+omjain2001@users.noreply.github.com>
2024-12-02 19:41:36 -0500

    Time: 0 ms (100.00%) | Memory: 8.9 MB (11.12%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,20 +2,18 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> st;
-        int i = 0;
-        int n = s.length();
-        for (auto i: s){
-            if(i == '(' || i == '{' || i == '[') st.push(i);
-            else if(st.empty()) return false;
-            else{
-                char ch = st.top();
+        unordered_map<char,char> m;
+        m[')'] = '(';
+        m[']'] = '[';
+        m['}'] = '{';
+        for(auto ele: s){
+            if(ele == '(' || ele == '{' || ele == '[') st.push(ele);
+            else {
+                if(st.empty() || m[ele] != st.top()) return false;
                 st.pop();
-                if((i == ')' && ch != '(') || (i == '}' && ch != '{') || (i == ']' && ch != '[')) return false;
             }
         }
-
         if(!st.empty()) return false;
-
         return true;
     }
 };
