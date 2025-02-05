5e4e8887778bcf0c6f1e9fd5189bab10c332c395
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-02-05 14:21:51 -0500

    Time: 0 ms (100.00%) | Memory: 10 MB (43.57%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,25 +1,62 @@
 class Solution {
 public:
+    bool checkEqual(int a[26], int b[26]){
+        for(int i=0; i<26; i++){
+            if(a[i] != b[i]) return false;
+        }
+        return true;
+    }
     bool checkInclusion(string s1, string s2) {
-        int n1 = s1.length(), n2 = s2.length();
-        if(n1 > n2) return false;
-        sort(s1.begin(), s1.end());
-        if(n1 == n2){
-            sort(s2.begin(), s2.end());
-            return s1 == s2;
-        }
+
+        // Complexity  = O(nlgn)
+
+        // int n1 = s1.length(), n2 = s2.length();
+        // if(n1 > n2) return false;
+        // sort(s1.begin(), s1.end());
+        // if(n1 == n2){
+        //     sort(s2.begin(), s2.end());
+        //     return s1 == s2;
+        // }
 
 
-        string temp = "";
-        for(int i=0; i<n2; i++){
-            temp += s2[i];
-            if(i >= n1-1){
-                string str = temp;
-                sort(str.begin(), str.end());
-                if(str == s1) return true;
-                temp = temp.substr(1,n1-1);
-            }
+        // string temp = "";
+        // for(int i=0; i<n2; i++){
+        //     temp += s2[i];
+        //     if(i >= n1-1){
+        //         string str = temp;
+        //         sort(str.begin(), str.end());
+        //         if(str == s1) return true;
+        //         temp = temp.substr(1,n1-1);
+        //     }
+        // }
+        // return false; 
+
+        
+        // Complexity = O(26 * n)
+
+        if(s1.length() > s2.length()) return false;
+        int a[26] = {0};
+        for(auto i: s1){
+            a[i-'a']++;
         }
-        return false; 
+
+        int b[26] = {0};
+        int i=0;
+        while(i < s1.length() && i < s2.length()){
+            b[s2[i]-'a']++;
+            i++;
+        }
+
+        if(checkEqual(a,b)) return true;
+
+        while(i < s2.length()){
+            b[s2[i]-'a']++;
+            b[s2[i-s1.length()]-'a']--;
+            if(checkEqual(a,b)) return true;
+            i++;
+        }
+        return false;
+
+
     }
 };
