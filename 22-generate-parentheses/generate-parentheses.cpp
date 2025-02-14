43b53413a1e1cc66fa06c898586774e6398bfba3
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-02-13 21:52:42 -0500

    Time: 12 ms (7.32%) | Memory: 23.2 MB (6.18%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void recur(string s, int i, string temp, vector<string> &ans){
+        if(temp.length() == (2*s.length())){
+            ans.push_back(temp);
+            return;
+        }
+        if(i == s.length()) return recur(s, i, temp + ')', ans);
+        if(temp.length() == 0) return recur(s, i+1, temp+'(', ans);
+        recur(s, i+1, temp+'(', ans);
+        recur(s, i, temp+')', ans);
+        return;
+    }
+    void recur_2(int open, int close, string temp, int n, vector<string> &ans){
+        if (close > open) return;
+        if(open == n && close == n){
+            ans.push_back(temp);
+            return;
+        }
+
+        if(open > n) return recur_2(open, close+1, temp+')', n, ans);
+        recur_2(open+1, close, temp+'(', n, ans);
+        recur_2(open, close+1, temp+')', n, ans);
+        return;
+    }
+    vector<string> generateParenthesis(int n) {
+        string s = "";
+        for(int i=0; i<n; i++) s+='(';
+        vector<string> ans;
+        // recur(s, 0, "", ans);
+        recur_2(0,0,"",n,ans);
+        return ans;
+    }
+};
