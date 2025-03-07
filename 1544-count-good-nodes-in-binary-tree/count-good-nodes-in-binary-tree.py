0812f29f8b0dc8c99d7f08441fdbd4fec00e50c2
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-03-07 15:09:17 -0500

    Time: 201 ms (5.23%) | Memory: 32.2 MB (63.41%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -10,4 +10,14 @@ class Solution:
         ans = 1 if (root.val >= maxi) else 0
         return ans + self.recur(root.left, max(root.val, maxi)) + self.recur(root.right, max(root.val, maxi))
     def goodNodes(self, root: TreeNode) -> int:
-        return self.recur(root, -10**5)
+        # return self.recur(root, -10**5)
+        if root == None: return 0
+        q = [(root, root.val)]
+        count = 0
+        while(len(q) > 0):
+            node, val = q[0]
+            q.pop(0)
+            if node.val >= val: count += 1
+            if node.left: q.append((node.left, max(node.val, val)))
+            if node.right: q.append((node.right, max(node.val, val)))
+        return count
