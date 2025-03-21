678cd6f155fbb878a73e2ed3aa1cc976c9d8163d
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-03-21 15:50:53 -0400

    Time: 19 ms (74.24%) | Memory: 18.3 MB (66.84%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -8,17 +8,17 @@ class Solution:
         
         def dfs(i,j):
             nonlocal maxi
+            if not grid:
+                return 0
             if i < 0 or i == r or j < 0 or j == c or grid[i][j] == 0:
                 return 0
             grid[i][j] = 0
-            area = 1 + dfs(i,j+1) + dfs(i+1,j) + dfs(i-1,j) + dfs(i,j-1)
-            maxi = max(maxi, area)
-            return area
+            return 1 + dfs(i,j+1) + dfs(i+1,j) + dfs(i-1,j) + dfs(i,j-1)
         
         for i in range(r):
             for j in range(c):
                 if grid[i][j] == 1:
-                    dfs(i,j)
+                    maxi = max(maxi, dfs(i,j))
         
         return maxi
         
