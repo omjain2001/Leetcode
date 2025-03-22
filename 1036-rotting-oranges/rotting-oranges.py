f38f5e5d5fdcb20bb318a72e2b8bdf0248c9d6b6
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-03-21 23:03:15 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (45.60%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -0,0 +1,48 @@
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+
+        if not grid:
+            return -1
+
+        r = len(grid)
+        c = len(grid[0])
+
+        queue = []
+        for i in range(r):
+            for j in range(c):
+                if grid[i][j] == 2:
+                    queue.append((i,j))
+        time = 0
+
+        while(len(queue) > 0):
+            count = len(queue)
+            isFresh = False
+            while(count):
+                i,j = queue.pop(0)
+                if i > 0 and grid[i-1][j] == 1:
+                    queue.append((i-1,j))
+                    grid[i-1][j] = 2
+                    isFresh = True
+                if i < r-1 and grid[i+1][j] == 1:
+                    queue.append((i+1,j))
+                    grid[i+1][j] = 2
+                    isFresh = True
+                if j > 0 and grid[i][j-1] == 1:
+                    queue.append((i,j-1))
+                    grid[i][j-1] = 2
+                    isFresh = True
+                if j < c-1 and grid[i][j+1] == 1:
+                    queue.append((i,j+1))
+                    grid[i][j+1] = 2
+                    isFresh = True
+                count -= 1
+            if isFresh:
+                time += 1
+        for i in range(r):
+            for j in range(c):
+                if grid[i][j] == 1:
+                    return -1
+        return time
+                
+                
+        
