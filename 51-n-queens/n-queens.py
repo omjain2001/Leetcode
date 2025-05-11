37dab7a9ad4f81952b038640da5fc878550db13a
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-05-11 16:29:58 -0400

    Time: 11 ms (75.46%) | Memory: 18.1 MB (83.04%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -3,16 +3,10 @@ class Solution:
         # Check upper column
         if dp_c[j] == 'Q':
             return False
-        # for r in range(i, -1, -1):
-        #     if board[r][j] == 'Q':
-        #         return False
         
         # Check left
         if dp_r[i] == 'Q':
             return False
-        # for c in range(j, -1, -1):
-        #     if board[i][c] == 'Q':
-        #         return False
         
         # Check left upper diagonal
         r = i
@@ -35,36 +29,42 @@ class Solution:
         # Valid position
         return True
 
-    def recur(self, board, ans, i, j, n, placed, dp_r, dp_c):
-        if placed == n:
+    def recur(self, board, ans, i, cols, left_dig, right_dig):
+        if i == len(board):
             temp = [''.join(ele) for ele in board]
-            # for r in range(n):
-            #     item = ""
-            #     for c in board[r]:
-            #         item += c
-            #     temp.append(item)
             ans.append(temp)
             return
-        
-        if i < 0 or i >= n or j < 0 or j >= n:
-            return
 
-        for p in range(i, n):
-            for q in range(j, n):
-                if board[p][q] != 'Q' and self.isValid(board, p, q, n, dp_r, dp_c):
-                    board[p][q] = 'Q'
-                    dp_r[p] = 'Q'
-                    dp_c[q] = 'Q'
-                    self.recur(board, ans, p+1, 0, n, placed + 1, dp_r, dp_c)
-                    board[p][q] = '.'
-                    dp_r[p] = '.'
-                    dp_c[q] = '.'
+        for j in range(0, len(board)):
+            if j in cols or (i+j) in right_dig or (i-j) in left_dig:
+                continue
+            cols.add(j)
+            right_dig.add(i+j)
+            left_dig.add(i-j)
+            board[i][j] = 'Q'
+            self.recur(board, ans, i+1, cols, left_dig, right_dig)
+            board[i][j] = '.'
+            left_dig.remove(i-j)
+            right_dig.remove(i+j)
+            cols.remove(j)
+            # for q in range(j, n):
+            #     if board[p][q] != 'Q' and self.isValid(board, p, q, n, dp_r, dp_c):
+            #         board[p][q] = 'Q'
+            #         dp_r[p] = 'Q'
+            #         dp_c[q] = 'Q'
+            #         self.recur(board, ans, p+1, 0, n, placed + 1, dp_r, dp_c)
+            #         board[p][q] = '.'
+            #         dp_r[p] = '.'
+            #         dp_c[q] = '.'
         
 
     def solveNQueens(self, n: int) -> List[List[str]]:
         ans = []
         board = [['.']*n for i in range(n)]
-        dp_r = ['.']*n
-        dp_c = ['.']*n
-        self.recur(board, ans, 0, 0, n, 0, dp_r, dp_c)
+        # dp_r = ['.']*n
+        # dp_c = ['.']*n
+        cols = set()
+        left_dig = set()
+        right_dig = set()
+        self.recur(board, ans, 0, cols, left_dig, right_dig)
         return ans
