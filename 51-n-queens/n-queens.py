5c30ee9e27669718e8872e83c09ce35a07142238
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-05-11 15:35:41 -0400

    Time: 3335 ms (5.00%) | Memory: 18.3 MB (56.46%) - LeetSync

---
+++ 51-n-queens/n-queens.py
@@ -0,0 +1,60 @@
+class Solution:
+    def isValid(self, board, i, j, n):
+        # Check upper column
+        for r in range(i, -1, -1):
+            if board[r][j] == 'Q':
+                return False
+        
+        # Check left
+        for c in range(j, -1, -1):
+            if board[i][c] == 'Q':
+                return False
+        
+        # Check left upper diagonal
+        r = i
+        c = j
+        while(r >= 0 and c >= 0):
+            if board[r][c] == 'Q':
+                return False
+            r -= 1
+            c -= 1
+        
+        r = i
+        c = j
+        # Check upper right diagonal
+        while(r >= 0 and c < n):
+            if board[r][c] == 'Q':
+                return False
+            r -= 1
+            c += 1
+        
+        # Valid position
+        return True
+
+    def recur(self, board, ans, i, j, n, placed):
+        if placed == n:
+            temp = []
+            for r in range(n):
+                item = ""
+                for c in board[r]:
+                    item += c
+                temp.append(item)
+            ans.append(temp)
+            return
+        
+        if i < 0 or i >= n or j < 0 or j >= n:
+            return
+
+        for p in range(i, n):
+            for q in range(j, n):
+                if board[p][q] != 'Q' and self.isValid(board, p, q, n):
+                    board[p][q] = 'Q'
+                    self.recur(board, ans, p+1, 0, n, placed + 1)
+                    board[p][q] = '.'
+        
+
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        ans = []
+        board = [['.']*n for i in range(n)]
+        self.recur(board, ans, 0, 0, n, 0)
+        return ans
