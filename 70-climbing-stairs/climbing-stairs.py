a759c2ed49ec300d46024720ae70fb261d5bafe2
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-05-25 14:54:49 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (48.52%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,19 +1,31 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
 
-        dp = [-1] * (n+1)
-        dp[n] = 1
+        # dp = [-1] * (n+1)
+        # dp[n] = 1
 
-        def recur(n, i):
-            if i > n:
-                return 0
+        # def recur(n, i):
+        #     if i > n:
+        #         return 0
 
-            if dp[i] != -1:
-                return dp[i]
+        #     if dp[i] != -1:
+        #         return dp[i]
             
-            dp[i] = recur(n, i+1) + recur(n, i+2)
+        #     dp[i] = recur(n, i+1) + recur(n, i+2)
             
-            return dp[i]
+        #     return dp[i]
         
-        return recur(n,0)
+        # Tabulation method
+        if n == 1:
+            return 1
+        
+        dp = [0] * (n+1)
+        dp[n] = 1
+        dp[n-1] = 1
+
+        for i in range(n-2, -1, -1):
+            dp[i] = dp[i+1] + dp[i+2]
+        
+        # return recur(n, 0)
+        return dp[0]
         
