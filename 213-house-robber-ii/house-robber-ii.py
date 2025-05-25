5c2b888f3734b858703942132b9bb05e9d75e40e
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-05-25 15:58:05 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (85.89%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -2,30 +2,49 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         
         n = len(nums)
-
         if n == 1:
             return nums[0]
+        if n == 2:
+            return max(nums)
         
         dp = [-1] * (n+1)
         dp[n] = 0
 
-        # Recursion and memoization
-        def recur(i, arr):
-            if i >= n-1:
-                return 0
-            if dp[i] != -1:
-                return dp[i]
+        # # Recursion and memoization
+        # def recur(i, arr):
+        #     if i >= n-1:
+        #         return 0
+        #     if dp[i] != -1:
+        #         return dp[i]
             
-            dp[i] = max(arr[i] + recur(i+2, arr), recur(i+1, arr))
+        #     dp[i] = max(arr[i] + recur(i+2, arr), recur(i+1, arr))
 
-            return dp[i]
+        #     return dp[i]
         
-        arr = nums[:-1]
-        ans1 = recur(0, arr)
+        # arr = nums[:-1]
+        # ans1 = recur(0, arr)
 
-        dp = [-1] * (n+1)
-        dp[n] = 0
-        arr = nums[1:]
-        ans2 = recur(0, arr)
+        # dp = [-1] * (n+1)
+        # dp[n] = 0
+        # arr = nums[1:]
+        # ans2 = recur(0, arr)
+        # return max(ans1, ans2)
+
+
+        # Tabulation
+        dp[n-1] = 0
+        for i in range(n-2, -1, -1):
+            dp[i] = max(nums[i] + dp[i+2], dp[i+1])
+        
+        ans1 = dp[0]
+
+        dp = [0] * (n+2)
+        for i in range(n-1, 0, -1):
+            dp[i] = max(nums[i] + dp[i+2], dp[i+1])
+        
+        ans2 = dp[1]
+
         return max(ans1, ans2)
+
+
         
