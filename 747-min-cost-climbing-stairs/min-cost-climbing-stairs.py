d6ef495e08892e23ef091d3a82fd59320d4f7f17
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-05-25 15:14:47 -0400

    Time: 7 ms (20.94%) | Memory: 19.2 MB (16.22%) - LeetSync

---
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+        n = len(cost)
+        dp = [-1] * (n+1)
+        dp[n] = 0
+
+        # Recursion and memoization
+        def recur(i):
+            if i > len(cost):
+                return 1e10
+            if dp[i] != -1:
+                return dp[i]
+            
+            dp[i] = cost[i] + min(recur(i+1), recur(i+2))
+
+            return dp[i]
+        
+        return min(recur(0), recur(1))
+
+
+        # # Tabulation
+        # dp[n-1] = cost[-1]
+        # dp[n-2] = cost[-2]
+
+        # for i in range(n-2, -1, -1):
+        #     dp[i] = cost[i] + min(dp[i+1], dp[i+2])
+        
+        # return dp[0]
+
+        
