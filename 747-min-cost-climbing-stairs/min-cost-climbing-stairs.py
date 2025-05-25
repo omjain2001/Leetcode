8139cd7ddc439770be74e33a180c83dad8edf6e4
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-05-25 15:17:29 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (62.59%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -5,27 +5,27 @@ class Solution:
         dp = [-1] * (n+1)
         dp[n] = 0
 
-        # Recursion and memoization
-        def recur(i):
-            if i > len(cost):
-                return 1e10
-            if dp[i] != -1:
-                return dp[i]
+        # # Recursion and memoization
+        # def recur(i):
+        #     if i > len(cost):
+        #         return 1e10
+        #     if dp[i] != -1:
+        #         return dp[i]
             
-            dp[i] = cost[i] + min(recur(i+1), recur(i+2))
+        #     dp[i] = cost[i] + min(recur(i+1), recur(i+2))
 
-            return dp[i]
+        #     return dp[i]
         
-        return min(recur(0), recur(1))
+        # return min(recur(0), recur(1))
 
 
-        # # Tabulation
-        # dp[n-1] = cost[-1]
-        # dp[n-2] = cost[-2]
+        # Tabulation
+        dp[n-1] = cost[-1]
+        dp[n-2] = cost[-2]
 
-        # for i in range(n-2, -1, -1):
-        #     dp[i] = cost[i] + min(dp[i+1], dp[i+2])
+        for i in range(n-2, -1, -1):
+            dp[i] = cost[i] + min(dp[i+1], dp[i+2])
         
-        # return dp[0]
+        return min(dp[0], dp[1])
 
         
