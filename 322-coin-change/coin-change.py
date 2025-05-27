e396b4cc905539bdbde82f0b392ba0d7c7de36fd
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-05-26 22:04:43 -0400

    Time: 875 ms (33.11%) | Memory: 18.2 MB (68.47%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,39 +1,25 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
 
-        dp = [[-1] * (amount + 1) for i in range(len(coins)+1)]
-        # for i in range(len(coins) + 1):
-        #     dp[i][0] = 0
-        # for i in range(amount + 1):
-        #     dp[0][i] = 0
-        
+        coins.sort()
 
-        def recur(i, rem):
+        dp = [1e10] * (amount+1)
+        dp[0] = 0
 
-            if rem == 0:
-                return 0
-
-            if i == (len(coins) - 1):
-                if rem % coins[i] == 0:
-                    return rem // coins[i]
-                return 1e10
+        for i in range(1, amount+1):
+            dp[i] = 1e10
             
-            if dp[i][rem] != -1:
-                return dp[i][rem]
+            for c in coins:
+                if i - c < 0:
+                    break
+                
+                if dp[i-c] != 1e10:
+                    dp[i] = min(dp[i], 1+dp[i-c])
 
-            take = 1e10
-            if rem >= coins[i]:
-                take = min(1 + recur(i, rem - coins[i]), 1e10)
-            not_take = recur(i+1, rem)
-            
-            dp[i][rem] = min(take, not_take)
-            return dp[i][rem]
         
-        
-        ans = recur(0, amount)
-        if ans == 1e10:
+        if dp[amount] == 1e10:
             return -1
-        return ans
+        return dp[amount]
         
 
         
