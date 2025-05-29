d74ffb29721da115f89daac6ce86863b8cae3e74
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-05-28 23:22:20 -0400

    Time: 1 ms (31.79%) | Memory: 17.6 MB (98.10%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -3,17 +3,31 @@ class Solution:
 
         dp = [[-1] * (n+1) for j in range(m+1)]
 
-        def recur(i, j):
-            if (i == m-1) or (j == n-1):
-                return 1
+        # def recur(i, j):
+        #     if (i == m-1) or (j == n-1):
+        #         return 1
             
-            if dp[i][j] != -1:
-                return dp[i][j]
+        #     if dp[i][j] != -1:
+        #         return dp[i][j]
             
-            dp[i][j] = recur(i+1, j) + recur(i, j+1)
+        #     dp[i][j] = recur(i+1, j) + recur(i, j+1)
             
-            return dp[i][j]
+        #     return dp[i][j]
         
-        return recur(0,0)
+        # return recur(0,0)
+
+        # Tabulation
+
+        for i in range(m):
+            dp[i][n-1] = 1
+        
+        for j in range(n):
+            dp[m-1][j] = 1
+        
+        for i in range(m-2, -1, -1):
+            for j in range(n-2, -1, -1):
+                dp[i][j] = dp[i+1][j] + dp[i][j+1]
+        
+        return dp[0][0]
 
         
