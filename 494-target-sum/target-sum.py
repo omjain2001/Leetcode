c3947f28576283a9215b57f2eb38b6198b5e4ace
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-05-29 21:50:21 -0400

    Time: 120 ms (65.48%) | Memory: 17.9 MB (80.79%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,19 +1,35 @@
+from collections import defaultdict
+
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
 
         dp = {}
 
-        def recur(i, tgt):
+        # Recursion and Memoization
+        # def recur(i, tgt):
 
-            if i >= len(nums):
-                return (tgt == target)
+        #     if i >= len(nums):
+        #         return (tgt == target)
 
-            if (i, tgt) in dp:
-                return dp[(i, tgt)]
+        #     if (i, tgt) in dp:
+        #         return dp[(i, tgt)]
             
-            dp[(i, tgt)] = recur(i+1, tgt + nums[i]) + recur(i+1, tgt - nums[i])
-            return dp[(i, tgt)]
+        #     dp[(i, tgt)] = recur(i+1, tgt + nums[i]) + recur(i+1, tgt - nums[i])
+        #     return dp[(i, tgt)]
         
-        return recur(0, 0)
+        # return recur(0, 0)
+
+
+        # Tabulation
+        dp = [defaultdict(int) for _ in range(len(nums) + 1)]
+        dp[0][0] = 1 # When nums is empty and target is 0, there is only one possible way
+
+        for i in range(len(nums)):
+            for curr_sum, count in dp[i].items():
+                dp[i+1][curr_sum - nums[i]] += count
+                dp[i+1][curr_sum + nums[i]] += count
+        
+        return dp[len(nums)][target]
+
             
         
