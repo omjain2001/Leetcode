46dbfbde076fa278da2bd66ce71f5e23e184acef
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-06-01 18:26:07 -0400

    Time: 47 ms (63.07%) | Memory: 18.4 MB (20.87%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -8,7 +8,7 @@ class Solution:
         if len(s3) != (n1+n2):
             return False
 
-        dp = [[-1] * (n2+1) for _ in range(n1+1)]
+        dp = [[False] * (n2+1) for _ in range(n1+1)]
 
         def recur(i, j):
             if n1 == 0:
@@ -24,10 +24,22 @@ class Solution:
             dp[i][j] = (int)((i < n1 and s1[i] == s3[i+j] and recur(i+1, j)) or (j < n2 and s2[j] == s3[i+j] and recur(i, j+1)))
             return dp[i][j]
 
-        ans = recur(0, 0)
-        if ans == 1:
-            return True
-        return False
+        # ans = recur(0, 0)
+        # if ans == 1:
+        #     return True
+        # return False
+
+        dp[n1][n2] = True
+
+        for i in range(n2-1, -1, -1):
+            dp[n1][i] = (s3[n1+i] == s2[i]) and dp[n1][i+1]
+        
+        for i in range(n1-1, -1, -1):
+            dp[i][n2] = (s3[i+n2] == s1[i]) and dp[i+1][n2]
+
+        for i in range(n1-1, -1, -1):
+            for j in range(n2-1, -1, -1):
+                dp[i][j] = (int)((s1[i] == s3[i+j] and dp[i+1][j]) or (s2[j] == s3[i+j] and dp[i][j+1]))
+        
+        return dp[0][0]
                 
-            
-            
