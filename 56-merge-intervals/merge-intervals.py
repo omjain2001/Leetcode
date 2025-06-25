710cc61cb4b23b2b5bda493dd7a8447bd8f6aa4a
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-06-25 16:31:53 -0400

    Time: 11 ms (30.12%) | Memory: 21.6 MB (34.50%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -9,13 +9,22 @@ class Solution:
         idx = 0
         ans = []
         while(idx < len(intervals)):
-            while(idx < len(intervals) and end >= intervals[idx][0]):
+            # while(idx < len(intervals) and end >= intervals[idx][0]):
+            #     end = max(end, intervals[idx][1])
+            #     idx += 1
+            # ans.append([start, end])
+            # if idx < len(intervals):
+            #     start = intervals[idx][0]
+            #     end = intervals[idx][1]
+            if intervals[idx][0] <= end:
                 end = max(end, intervals[idx][1])
-                idx += 1
-            ans.append([start, end])
-            if idx < len(intervals):
+            else:
+                ans.append([start, end])
                 start = intervals[idx][0]
                 end = intervals[idx][1]
+            idx += 1
+        
+        ans.append([start, end])
         
         return ans
             
