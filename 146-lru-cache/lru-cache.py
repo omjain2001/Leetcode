9a2d0aa403c464a97c82af284a5fe19eb1b9f18c
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-06-25 01:43:08 -0400

    Time: 145 ms (50.34%) | Memory: 78.7 MB (27.50%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,5 +1,15 @@
 import heapq
 from collections import defaultdict
+
+class Node:
+    def __init__(self, key, val):
+        self.val = val
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+# Using large space complexity
 class LRUCache:
 
     def __init__(self, capacity: int):
@@ -33,6 +43,44 @@ class LRUCache:
         self.cache[key] = (value, self.time)
         heapq.heappush(self.pq, (self.time, key))
 
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.keys = {}
+        self.head = Node(0,0)
+        self.tail = Node(0,0)
+        self.head.right, self.tail.left = self.tail, self.head
+    
+    def removeNode(self, node):
+        node.left.right, node.right.left = node.right, node.left
+        node.left = node.right = None
+    
+    def insertNode(self, key, val):
+        node = Node(key, val)
+        node.left, node.right = self.head, self.head.right
+        node.left.right = node.right.left = node
+        return node
+
+    def get(self, key: int) -> int:
+        if key not in self.keys:
+            return -1
+        val = self.keys[key].val
+        self.removeNode(self.keys[key])
+        self.keys[key] = self.insertNode(key, val)
+        return val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.keys:
+            self.keys[key].val = value
+            self.removeNode(self.keys[key])
+            self.keys[key] = self.insertNode(key, value)
+        elif len(self.keys) < self.cap:
+            self.keys[key] = self.insertNode(key, value)
+        else:
+            del self.keys[self.tail.left.key]
+            self.removeNode(self.tail.left)
+            self.keys[key] = self.insertNode(key, value)
 
 
 # Your LRUCache object will be instantiated and called as such:
