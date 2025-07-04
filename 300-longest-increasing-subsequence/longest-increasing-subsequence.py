76705a35747e5f8c901f4813fc409eb46a7cce6b
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-07-04 15:57:34 -0400

    Time: 7 ms (83.74%) | Memory: 18.2 MB (37.68%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,13 +1,39 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [1] * len(nums)
+
+        # TC => O(n^2)
+
+        # dp = [1] * len(nums)
+
+        # for i in range(len(nums)):
+        #     for j in range(0, i):
+        #         if nums[j] < nums[i] and dp[i] < dp[j] + 1:
+        #             dp[i] = dp[j] + 1
+
+        # return max(dp)
+
+
+        # TC => O(nlgn)
+
+        arr = []
 
         for i in range(len(nums)):
-            for j in range(0, i):
-                if nums[j] < nums[i] and dp[i] < dp[j] + 1:
-                    dp[i] = dp[j] + 1
-
-        return max(dp)
+            if (len(arr) == 0) or (nums[i] > arr[-1]):
+                arr.append(nums[i])
+            else:
+                # Binary search
+                l = 0
+                r = len(arr)-1
+                while(l <= r):
+                    m = (l + r) // 2
+                    if nums[i] > arr[m]:
+                        l = m + 1
+                    else:
+                        r = m - 1
+                
+                arr[l] = nums[i]
+        
+        return len(arr)
                 
             
         
