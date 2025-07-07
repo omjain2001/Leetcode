73070acf27d4991f144316c4b82be640b4150286
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-07-07 14:44:16 -0400

    Time: 0 ms (100.00%) | Memory: 18.4 MB (9.77%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.py
@@ -4,23 +4,20 @@ class Solution:
         n = len(nums)
 
         while l <= r:
-            
-            while l < r and nums[l+1] == nums[l]:
-                l += 1
-            
-            while r > l and nums[r] == nums[r-1]:
-                r -= 1
-
             m = (l + r) // 2
             if nums[m] == target:
                 return True
-            elif nums[0] <= nums[m]:
-                if nums[0] <= target < nums[m]:
+            elif (nums[m] == nums[l]) and (nums[m] == nums[r]):
+                l += 1
+                r -= 1
+                continue
+            elif nums[l] <= nums[m]:
+                if nums[l] <= target < nums[m]:
                     r = m - 1
                 else:
                     l = m + 1
             else:
-                if nums[m] < target <= nums[n - 1]:
+                if nums[m] < target <= nums[r]:
                     l = m + 1
                 else:
                     r = m - 1
