b4c680f534c7e6e38b04effffd8fa5bedb77dd26
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-07-15 16:07:18 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (41.69%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -1,18 +1,21 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        ans = []
-        hashmap = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}
 
-        temp = []
+        hashmap = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+
+        ans = []
         for d in digits:
-            for i in hashmap[d]:
+            temp = []
+            for ch in hashmap[d]:
                 if len(ans) > 0:
-                    for j in ans:
-                        temp.append(j + i)
+                    for i in ans:
+                        temp.append(i + ch)
                 else:
-                    temp.append(i)
+                    temp.append(ch)
+            
             ans = temp.copy()
             temp = []
-        return ans
         
+        return ans
+            
         
