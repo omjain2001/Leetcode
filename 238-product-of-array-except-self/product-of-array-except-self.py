dedb6a887a4d7e3a42e7157ce0452825aaace8ec
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-08-19 17:56:39 -0400

    Time: 27 ms (46.00%) | Memory: 27 MB (19.50%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -1,30 +1,25 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prod = 1
-        zc = 0
-        for i in nums:
-            if i != 0:
-                prod *= i
-            else:
-                zc += 1
         
-        ans = [0] * len(nums)
+        if len(nums) == 0:
+            return []
 
-        if zc > 1:
-            return ans
-        elif zc == 1:
-            ans[nums.index(0)] = prod
-            return ans
-        else:
-            lp = [0] * len(nums)
-            rp = [0] * len(nums)
-            lp[0] = 1
-            rp[len(nums)-1] = 1
-            
-            for i in range(1, len(nums)):
-                lp[i] = lp[i-1] * nums[i-1]
-            for i in range(len(nums)-2, -1, -1):
-                rp[i] = rp[i+1] * nums[i+1]
-            for i in range(len(nums)):
-                ans[i] = lp[i] * rp[i]
-            return ans
+        pre = [1] * len(nums)
+        suf = [1] * len(nums)
+
+        prod = nums[0]
+        for idx in range(1, len(nums)):
+            pre[idx] = prod
+            prod *= nums[idx]
+
+        prod = nums[-1]
+        for idx in range(len(nums)-2, -1, -1):
+            suf[idx] = prod
+            prod *= nums[idx]
+
+        ans = []
+        for idx in range(len(nums)):
+            ans.append(pre[idx] * suf[idx])
+        
+        return ans
+        
