480bee4f085062b8f4f0208a3bfe11ed81d86704
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-08-20 00:23:15 -0400

    Time: 199 ms (25.60%) | Memory: 27.7 MB (29.03%) - LeetSync

---
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        dp = [[[-1] * (k+1) for _ in range(2)] for _ in range(len(prices)+1)]
+
+        def recur(i, buy, count):
+            if i >= len(prices) or count == 0:
+                return 0
+
+            if dp[i][buy][count] != -1:
+                return dp[i][buy][count]
+            
+            ans = 0
+            if buy:
+                ans = max(-prices[i] + recur(i+1, 0, count), 0 + recur(i+1, 1, count))
+            else:
+                ans = max(prices[i] + recur(i+1, 1, count-1), 0 + recur(i+1, 0, count))
+            
+            dp[i][buy][count] = ans
+
+            return ans
+        
+        result = recur(0, 1, k)
+        return result
+        
