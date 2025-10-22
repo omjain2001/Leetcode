a11c636f1cda119bcf6fa3b608272a0f8b3c63e6
Om Jain <60605251+omjain2001@users.noreply.github.com>
2025-10-22 00:57:47 -0400

    Time: 181 ms (15.21%) | Memory: 78.6 MB (12.93%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,85 +1,86 @@
-import heapq
-from collections import defaultdict
-
 class Node:
     def __init__(self, key, val):
-        self.val = val
         self.key = key
-        self.left = None
-        self.right = None
+        self.val = val
+        self.next = None
+        self.prev = None
 
-
-# Using large space complexity
 class LRUCache:
 
     def __init__(self, capacity: int):
+        # Store capacity
         self.capacity = capacity
-        self.cache = defaultdict(tuple)
-        self.pq = []
-        heapq.heapify(self.pq)
-        self.time = 0
-
-    def get(self, key: int) -> int:
-        self.time += 1
-        if key in self.cache:
-            self.cache[key] = (self.cache[key][0], self.time)
-            heapq.heappush(self.pq, (self.time, key))
-            return self.cache[key][0]
-        return -1
-
-    def put(self, key: int, value: int) -> None:
         
-        self.time += 1
-        # Check capacity
-        if (len(self.cache) == self.capacity) and key not in self.cache:
-            # print('Working for key: ', key)
-            while((len(self.pq) > 0) and (self.cache[self.pq[0][1]][1] > self.pq[0][0])):
-                heapq.heappop(self.pq)
+        # Create dummy head and tail
+        self.head = Node(None, None)
+        self.tail = Node(None, None)
 
-            # print(self.pq)
-            t, recently_used_key = heapq.heappop(self.pq)
-            del self.cache[recently_used_key]
+        # Connect head and tail
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
-        self.cache[key] = (value, self.time)
-        heapq.heappush(self.pq, (self.time, key))
+        # Create a hashmap
+        self.hashmap = {}
 
-class LRUCache:
-
-    def __init__(self, capacity: int):
-        self.cap = capacity
-        self.keys = {}
-        self.head = Node(0,0)
-        self.tail = Node(0,0)
-        self.head.right, self.tail.left = self.tail, self.head
     
-    def removeNode(self, node):
-        node.left.right, node.right.left = node.right, node.left
-        node.left = node.right = None
+    def insert_node(self, key, val):
+        newnode = Node(key, val)
+        newnode.prev = self.head
+        newnode.next = self.head.next
+        self.head.next.prev = newnode
+        self.head.next = newnode
+
+        self.hashmap[key] = newnode
     
-    def insertNode(self, key, val):
-        node = Node(key, val)
-        node.left, node.right = self.head, self.head.right
-        node.left.right = node.right.left = node
-        return node
+
+    def remove_node(self, key):
+        deprecated_node = self.hashmap[key]
+        deprecated_node.prev.next = deprecated_node.next
+        deprecated_node.next.prev = deprecated_node.prev
+
+        self.hashmap.pop(key)
+
+        deprecated_node = None
+
 
     def get(self, key: int) -> int:
-        if key not in self.keys:
+
+        # Check if the value is present in the cache
+        if key in self.hashmap:
+
+            # Store the value
+            val = self.hashmap[key].val
+            
+            # Remove the key from the cache
+            self.remove_node(key)
+
+            # Insert the key as in the cache
+            self.insert_node(key, val)
+
+            return val
+        else:
             return -1
-        val = self.keys[key].val
-        self.removeNode(self.keys[key])
-        self.keys[key] = self.insertNode(key, val)
-        return val
+
 
     def put(self, key: int, value: int) -> None:
-        if key in self.keys:
-            self.removeNode(self.keys[key])
-            del self.keys[key]
-        
-        if len(self.keys) == self.cap:
-            del self.keys[self.tail.left.key]
-            self.removeNode(self.tail.left)
 
-        self.keys[key] = self.insertNode(key, value)
+        if key in self.hashmap:
+
+            # Remove and insert the node
+            self.remove_node(key)
+            self.insert_node(key, value)
+        
+        elif len(self.hashmap) < self.capacity:
+            self.insert_node(key, value)
+        
+        else:
+            node = self.tail.prev
+            old_key, old_val = node.key, node.val
+            self.remove_node(old_key)
+            self.insert_node(key, value)
+        
+
+        
 
 
 # Your LRUCache object will be instantiated and called as such:
