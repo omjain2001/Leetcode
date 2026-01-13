d7956832c5d7049366ca3e4b0b26ce576415ed4f
Om Jain <60605251+omjain2001@users.noreply.github.com>
2026-01-12 23:35:41 -0500

    Time: 2 ms (95.16%) | Memory: 22.4 MB (7.45%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -5,15 +5,26 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    d = 0
-    def recur(self, root: Optional[TreeNode]):
-        if root == None: return 0
-        left = self.recur(root.left)
-        right = self.recur(root.right)
-        self.d = max(self.d, left+right)
-        return 1 + max(left,right)
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        self.recur(root)
-        return self.d
 
+        # For each node, the path will be left + root + right.
+        # return 1 + max(left, right)
+
+        maxi = 0
+
+        def recur(node):
+            nonlocal maxi
+            if node is None:
+                return 0
+            
+            left = recur(node.left)
+            right = recur(node.right)
+            
+            maxi = max(maxi, 1 + left + right)
+
+            return 1 + max(left, right)
+        
+        recur(root)
+
+        return maxi-1
         
