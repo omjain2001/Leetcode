3a6ea05023fa9d45b84417f234287f720a450c68
Om Jain <60605251+omjain2001@users.noreply.github.com>
2026-01-25 00:05:02 -0500

    Time: 793 ms (43.67%) | Memory: 19.7 MB (11.37%) - LeetSync

--- 277-find-the-celebrity/find-the-celebrity.py
+++ 277-find-the-celebrity/find-the-celebrity.py
@@ -4,26 +4,39 @@
 
 class Solution:
     def findCelebrity(self, n: int) -> int:
-        # Brute force
-        celebrity = -1
-        for i in range(n):
-            is_celebrity = True
-            for j in range(n):
-                if j != i and not knows(j,i):
-                    is_celebrity = False
-                    break
-            
-            if is_celebrity:
-                celebrity = i
-                break
-
-        # Check if the celebrity does not know anyone
-        if celebrity == -1:
-            return -1
-        else:
-            for i in range(n):
-                if i != celebrity and knows(celebrity,i):
-                    return -1
         
+        # # Brute force
+        # celebrity = -1
+        # for i in range(n):
+        #     is_celebrity = True
+        #     for j in range(n):
+        #         if j != i and not knows(j,i):
+        #             is_celebrity = False
+        #             break
+            
+        #     if is_celebrity:
+        #         celebrity = i
+        #         break
+
+        # ## Verify if the celebrity does not know anyone
+        # if celebrity == -1:
+        #     return -1
+        # else:
+        #     for i in range(n):
+        #         if i != celebrity and knows(celebrity,i):
+        #             return -1
+        
+        # return celebrity
+
+        # Better approach
+        celebrity = 0
+        for i in range(1,n):
+            if not knows(i,celebrity):
+                # Since i knows celebrity, i is not a celebrity. Else, i can be a celebrity
+                celebrity = i
+        
+        for i in range(n):
+            if i != celebrity and (knows(celebrity, i) or not knows(i,celebrity)):
+                return -1
         return celebrity
         
