6dcdd4f97081118fb9e4c99cefee3c355606fe5a
Om Jain <60605251+omjain2001@users.noreply.github.com>
2026-02-15 10:42:01 -0500

    Time: 7 ms (65.42%) | Memory: 20.7 MB (12.17%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.py
+++ 10-regular-expression-matching/regular-expression-matching.py
@@ -13,7 +13,12 @@ class Solution:
             
             if dp[i][j] != -1:
                 return dp[i][j]
-            
+
+
+            # Intuition
+            # If it is direct match or '.', move to next characters
+            # If not, check if the next character is '*'. In this case, we can either ignore it and 
+            # move j by 2 (to next valid character) or consider it and move i to next character (here j still remains at the same character)
             match = i < len(s) and (s[i] == p[j] or p[j] == '.')
             if (j+1) < len(p) and p[j+1] == '*':
                 dp[i][j] = dfs(i,j+2) or (match and dfs(i+1,j))
@@ -22,6 +27,9 @@ class Solution:
             if match:
                 dp[i][j] = dfs(i+1,j+1)
                 return dp[i][j]
+
+            # In the case where i == len(s) but j < len(p), the only option for the string to match is all subsequent characters of j
+            # should have * after each character. The match condition will be False and the "If" condition determines the result.
             
             dp[i][j] = False
             return dp[i][j]
