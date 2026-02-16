72690cd515d8c92f286262b063d270797ccd37dc
Om Jain <60605251+omjain2001@users.noreply.github.com>
2026-02-15 19:54:53 -0500

    Time: 7 ms (62.52%) | Memory: 20 MB (64.39%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,37 +1,17 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
 
-        # DP Approach (O(n^2))
-
-        # dp = [1e10] * len(nums)
-
-        # def recur(i):
-        #     if i >= len(nums):
-        #         return 1e10
-        #     if i >= len(nums)-1:
-        #         return 0
-        #     if dp[i] != 1e10:
-        #         return dp[i]
-
-        #     mini = 1e10
-        #     for j in range(i+1, i+nums[i]+1):
-        #         mini = min(mini, 1 + recur(j))
-        #     dp[i] = mini
-        #     return dp[i]
-        
-        # ans = recur(0)
-        # return ans
-
-
-        # Linear Approach (O(n))
-        jumps = l = r = 0
-        while(r < len(nums)-1):
-            farthest = 0
+        l = r = 0
+        count = 0
+        while(l <= r and r < len(nums)-1):
+            temp = r
             for i in range(l, r+1):
-                farthest = max(farthest, i+nums[i])
-            l = r+1
-            r = farthest
-            jumps += 1
-        return jumps
-
+                temp = max(temp, i + nums[i])
+            count += 1
+            l = r + 1
+            r = temp
+        
+        if r >= len(nums)-1:
+            return count
+        return -1
         
