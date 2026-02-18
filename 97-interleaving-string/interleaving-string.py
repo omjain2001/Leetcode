39abef3cc4c7885b558d5fb4897dc4627cf29553
Om Jain <60605251+omjain2001@users.noreply.github.com>
2026-02-17 23:00:29 -0500

    Time: 52 ms (42.61%) | Memory: 20.2 MB (28.21%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -1,45 +1,33 @@
-from collections import Counter
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
 
-        n1 = len(s1)
-        n2 = len(s2)
-
-        if len(s3) != (n1+n2):
-            return False
-
-        dp = [[False] * (n2+1) for _ in range(n1+1)]
-
+        dp = [[-1]*len(s2) for _ in range(len(s1))]
+        
         def recur(i, j):
-            if n1 == 0:
-                return s3 == s2
-            if n2 == 0:
-                return s3 == s1
-            if i >= n1 and j >= n2:
+            if ((i+j) == len(s3)):
                 return True
+            if i == len(s1):
+                return s2[j:] == s3[i+j:]
+            if j == len(s2):
+                return s1[i:] == s3[i+j:]
 
             if dp[i][j] != -1:
                 return dp[i][j]
             
-            dp[i][j] = (int)((i < n1 and s1[i] == s3[i+j] and recur(i+1, j)) or (j < n2 and s2[j] == s3[i+j] and recur(i, j+1)))
+            k = i+j
+            if s3[k] == s1[i] and s3[k] == s2[j]:
+                dp[i][j] = recur(i, j+1) or recur(i+1, j)
+            elif s3[k] == s1[i]:
+                dp[i][j] = recur(i+1, j)
+            elif s3[k] == s2[j]:
+                dp[i][j] = recur(i, j+1)
+            else:
+                dp[i][j] = False
+            
             return dp[i][j]
-
-        # ans = recur(0, 0)
-        # if ans == 1:
-        #     return True
-        # return False
-
-        dp[n1][n2] = True
-
-        for i in range(n2-1, -1, -1):
-            dp[n1][i] = (s3[n1+i] == s2[i]) and dp[n1][i+1]
         
-        for i in range(n1-1, -1, -1):
-            dp[i][n2] = (s3[i+n2] == s1[i]) and dp[i+1][n2]
-
-        for i in range(n1-1, -1, -1):
-            for j in range(n2-1, -1, -1):
-                dp[i][j] = (int)((s1[i] == s3[i+j] and dp[i+1][j]) or (s2[j] == s3[i+j] and dp[i][j+1]))
+        if len(s3) != len(s1) + len(s2):
+            return False
+        return recur(0, 0)
+            
         
-        return dp[0][0]
-                
