bbe52188c7ec460be3ffdcf11398f2f20024b24a
Om Jain <60605251+omjain2001@users.noreply.github.com>
2026-02-19 07:23:24 -0500

    Time: 55 ms (23.28%) | Memory: 35.4 MB (9.95%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,25 +1,29 @@
 class Solution:
-    def isPalindrome(self, s, start, end):
-        while(start <= end):
-            if s[start] != s[end]:
-                return False
-            start += 1
-            end -= 1
-        return True
-
-    def recur(self, s, idx, temp, ans):
-        if idx == len(s):
-            ans.append(temp.copy())
-            return
-        
-        for i in range(idx, len(s)):
-            if self.isPalindrome(s, idx, i):
-                temp.append(s[idx: i+1])
-                self.recur(s, i+1, temp, ans)
-                temp.pop()
-        
     def partition(self, s: str) -> List[List[str]]:
-        ans = []
-        self.recur(s, 0, [], ans)
-        return ans
+
+        result = []
+        temp = []
+
+        def is_palindrome(i, j):
+            while(i <= j and s[i] == s[j]):
+                i += 1
+                j -= 1
+            if i <= j:
+                return False
+            return True
+
+        def recur(i):
+            if i == len(s):
+                result.append([ele for ele in temp])
+                return
+            
+            for j in range(i, len(s)):
+                if is_palindrome(i, j):
+                    temp.append(s[i:j+1])
+                    recur(j+1)
+                    temp.pop()
+        
+        recur(0)
+        return result
+
         
