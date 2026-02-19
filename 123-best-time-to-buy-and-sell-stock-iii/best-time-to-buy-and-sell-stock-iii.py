8244f268c32291b301909bc17481976a5a261856
Om Jain <60605251+omjain2001@users.noreply.github.com>
2026-02-19 06:40:23 -0500

    Time: 1495 ms (14.39%) | Memory: 119.8 MB (12.46%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,29 +1,25 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
-        dp = [[[-1] * 3 for _ in range(2)] for _ in range(len(prices)+1)]
+        dp = [[[-1]*3 for _ in range(2)]  for _ in range(len(prices))]
+
 
         def recur(i, buy, count):
-            if i >= len(prices) or count == 0:
+            
+            # Base conditions
+            if i == len(prices) or count == 0:
                 return 0
-
+            
             if dp[i][buy][count] != -1:
                 return dp[i][buy][count]
             
-            ans = 0
+            # Buy
             if buy:
-                ans = max(-prices[i] + recur(i+1, 0, count), 0 + recur(i+1, 1, count))
+                dp[i][buy][count] = max((-prices[i] + recur(i+1, 0, count)), recur(i+1, buy, count))
             else:
-                ans = max(prices[i] + recur(i+1, 1, count - 1), 0 + recur(i+1, 0, count))
+                dp[i][buy][count] = max((prices[i] + recur(i+1, 1, count-1)), recur(i+1, buy, count))
             
-            dp[i][buy][count] = ans
+            return dp[i][buy][count]
 
-            return ans
-        
-        result = recur(0, 1, 2)
-        return result
-        
-
-
-            
+        return recur(0, 1, 2)    
         
